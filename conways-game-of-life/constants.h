ca2dc6487a940b5fc09aa09e000c3a329573188a
qcrg <david-isupov@bk.ru>
2022-07-27 18:38:19 +0000

    removed old implementation

--- conways-game-of-life/constants.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr size_t MAX_W = 1000;
-constexpr size_t MAX_H = 1000;
-
-constexpr size_t DEFAULT_SCREEN_SIZE_W = 640;
-constexpr size_t DEFAULT_SCREEN_SIZE_H = 480;
--- conways-game-of-life/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Game.h"
-
-int main(int argc, char* argv[])
-{
-	Game g;
-	g.runGame();
-	return 0;
-}
